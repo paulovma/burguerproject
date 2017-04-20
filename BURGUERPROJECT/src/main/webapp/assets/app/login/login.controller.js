0bca247e09229b39d8111efd46ad7c8cf2959d64
Paulo <paulosilvav@gmail.com>
2017-04-20 17:42:56 -0300

    Login

--- BURGUERPROJECT/src/main/webapp/assets/app/login/login.controller.js
+++ BURGUERPROJECT/src/main/webapp/assets/app/login/login.controller.js
@@ -1,18 +1,12 @@
 (function() {
 	var CONTROLLER = ['$scope', 'LoginService', 'UserService', '$state', '$location', function($scope, LoginService, UserService, $state, $location) {
-		console.log('oi');
 		$scope.isRotaLogin = true;
-		var dados = {
-			usuario: "TESTE",
-			senha: "123"
-		}
-
+		
 		$scope.logar = function() {
-			console.log("OI");
+			var dados = angular.copy($scope.login);
 			LoginService.logar(dados).then(function(response) {
 				console.log(response);
-				if (response.data) {
-					console.log(response.data.token);
+				if (response.status == 200) {
 					UserService.setUser(response.data);
 					$scope.isRotaLogin = false;
 					$state.go('realizarpedido');
